f6665540db6ed675f162e41526b799a93010ecaf
Taehyun Kim <k1m743hyun@gmail.com>
2021-02-11 04:14:51 +0000

    Create 11021.java

---
+++ baekjoon-online-judge/Implementation/11021.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String args[]) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+
+    int a, b;
+    for (int i = 1; i < n + 1; i++) {
+      a = scanner.nextInt();
+      b = scanner.nextInt();
+      System.out.printf("Case #%d: %d\n", i, a + b);
+    }
+  }
+}
